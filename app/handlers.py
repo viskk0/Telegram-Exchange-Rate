2a08d7f09dbce2a495799a6a89751c230b3b2d51
Kuzmin Maxim <mail.worm.games@gmail.com>
2026-07-26 16:40:00 +0400

    Implement USD, RUB, KZT, EUR handlers with ExchangeRate-API integration

--- app/handlers.py
+++ app/handlers.py
@@ -17,12 +17,29 @@ async def init_db():
         async with db.execute("""CREATE TABLE IF NOT EXISTS exchange_rates (
     base TEXT,
     target TEXT,
-    rates_json REAL,
+    rates_json TEXT,
     updated_at INTEGER,
     PRIMARY KEY (base, target)
 )"""):
             await db.commit()
 
+
+async def getValute(name, message : Message):
+    async with aiosqlite.connect("exchange_rate.db") as db:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"{base_url}/{name}") as response:
+                if response.status == 200:
+                    api_data = await response.json()
+                    rates_dict = api_data['conversion_rates']
+                    updated_at = api_data['time_last_update_unix']
+    
+                    await db.execute("""INSERT OR REPLACE INTO exchange_rates (base, rates_json, updated_at) 
+                        VALUES (?, ?, ?)""", (name, json.dumps(rates_dict), updated_at))
+                    await db.commit()
+                    return rates_dict
+                else:
+                    await message.answer("Ошибка при запросе к API.")
+                    return
 
 @router.message(CommandStart())
 async def cmd_start(message : Message):
@@ -39,24 +56,71 @@ async def getUSD(message : Message):
 
         if row and (current_time - row['updated_at'] < 3600):
             data = json.loads(row['rates_json'])
-            print(data['RUB'])
         else:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(f"{base_url}/USD") as response:
-                    if response.status == 200:
-                        api_data = await response.json()
-                        rates_dict = api_data['conversion_rates']
-                        updated_at = api_data['time_last_update_unix']
-
-                        await db.execute("""INSERT OR REPLACE INTO exchange_rates (base, rates_json, updated_at) 
-                            VALUES (?, ?, ?)""", ("USD", json.dumps(rates_dict), updated_at))
-                        await db.commit()
-                        data = rates_dict
-                    else:
-                        await message.answer("Ошибка при запросе к API.")
-                        return
-
+            data = await getValute("USD", message)
+            
+        if not data: return
         usd_to_rub = data.get("RUB")
         usd_to_eur = data.get("EUR")
         usd_to_kzt = data.get("KZT")
-        await message.answer(f"Курс 1 USD: \n{usd_to_rub} RUB\n{usd_to_eur} EUR\n{usd_to_kzt} KZT")
+        await message.answer(f"Курс💲1 USD: \n🪙{usd_to_rub:.2f} RUB\n💶{usd_to_eur:.2f} EUR\n ₸{usd_to_kzt:.2f} KZT")
+
+@router.message(F.text == "🪙Получить курс рубля!")
+async def getRUB(message : Message):
+    async with aiosqlite.connect("exchange_rate.db") as db:
+        db.row_factory = aiosqlite.Row
+        async with db.execute("SELECT rates_json, updated_at FROM exchange_rates WHERE base = ?", ("RUB", )) as c:
+            row = await c.fetchone()
+
+        current_time = int(time.time())
+
+        if row and (current_time - row['updated_at'] < 3600):
+            data = json.loads(row['rates_json'])
+        else:
+            data = await getValute("RUB", message)
+
+        if not data: return
+        rub_to_usd = data.get("USD") * 100
+        rub_to_eur = data.get("EUR") * 100
+        rub_to_kzt = data.get("KZT") * 100
+        await message.answer(f"Курс 🪙100 RUB: \n💲{rub_to_usd:.2f} USD\n💶{rub_to_eur:.2f} EUR\n ₸{rub_to_kzt:.2f} KZT")
+
+@router.message(F.text == "₸Получить курс тенге!")
+async def getKZT(message : Message):
+    async with aiosqlite.connect("exchange_rate.db") as db:
+        db.row_factory = aiosqlite.Row
+        async with db.execute("SELECT rates_json, updated_at FROM exchange_rates WHERE base = ?", ("KZT", )) as c:
+            row = await c.fetchone()
+
+        current_time = int(time.time())
+
+        if row and (current_time - row['updated_at'] < 3600):
+            data = json.loads(row['rates_json'])
+        else:
+            data = await getValute("KZT", message)
+
+        if not data: return
+        kzt_to_usd = data.get("USD") * 1000
+        kzt_to_rub = data.get("RUB") * 1000
+        kzt_to_eur = data.get("EUR") * 1000
+        await message.answer(f"Курс ₸1000 KZT: \n💲{kzt_to_usd:.2f} USD\n🪙{kzt_to_rub:.2f} RUB\n💶{kzt_to_eur:.2f} EUR")
+
+@router.message(F.text == "💶Получить курс евро!")
+async def getEUR(message : Message):
+    async with aiosqlite.connect("exchange_rate.db") as db:
+        db.row_factory = aiosqlite.Row
+        async with db.execute("SELECT rates_json, updated_at FROM exchange_rates WHERE base = ?", ("EUR", )) as c:
+            row = await c.fetchone()
+
+        current_time = int(time.time())
+
+        if row and (current_time - row['updated_at'] < 3600):
+            data = json.loads(row['rates_json'])
+        else:
+            data = await getValute("EUR", message)
+
+        if not data: return
+        eur_to_usd = data.get("USD")
+        eur_to_rub = data.get("RUB")
+        eur_to_kzt = data.get("KZT")
+        await message.answer(f"Курс 💶1 EUR: \n💲{eur_to_usd:.2f} USD\n🪙{eur_to_rub:.2f} RUB\n ₸{eur_to_kzt:.2f} KZT")
--- app/keyboards.py
+++ app/keyboards.py
@@ -2,5 +2,6 @@ from aiogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, KeyboardBut
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
 MainKeyboard = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text="💲Получить курс доллара!"), KeyboardButton(text="🪙Получить курс рубля!")]
+    [KeyboardButton(text="💲Получить курс доллара!"), KeyboardButton(text="🪙Получить курс рубля!")],
+    [KeyboardButton(text="₸Получить курс тенге!"), KeyboardButton(text='💶Получить курс евро!')]
 ], resize_keyboard=True)
